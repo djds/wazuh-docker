8944040602dff7865ca4f6e9f1f15e05192b2110
Davis Schirmer <davis.schirmer@clearballot.com>
2021-01-18 18:23:10 -0500

    Refactor welcome_wazuh.sh

--- kibana-odfe/config/welcome_wazuh.sh
+++ kibana-odfe/config/welcome_wazuh.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
+
 # Wazuh Docker Copyright (C) 2021 Wazuh Inc. (License GPLv2)
 
-if [[ $CHANGE_WELCOME == "true" ]]
-then
-    echo "Set Wazuh app as the default landing page"
-    echo "server.defaultRoute: /app/wazuh" >> /usr/share/kibana/config/kibana.yml
+set -euo pipefail
 
-    echo "Set custom welcome styles"
+if [[ "${CHANGE_WELCOME}" = "true" ]]; then
+    printf "Set Wazuh app as the default landing page\n"
+    printf "server.defaultRoute: /app/wazuh\n" >>'/usr/share/kibana/config/kibana.yml'
+
+    printf "Set custom welcome styles\n"
     cp -f /tmp/custom_welcome/template.js.hbs /usr/share/kibana/src/legacy/ui/ui_render/bootstrap/template.js.hbs
     cp -f /tmp/custom_welcome/light_theme.style.css /usr/share/kibana/src/core/server/core_app/assets/legacy_light_theme.css
     cp -f /tmp/custom_welcome/*svg /usr/share/kibana/src/core/server/core_app/assets/
 fi
-
